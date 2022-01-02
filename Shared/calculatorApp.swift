380393af8919738f1168bf415b57bf7dffea4857
Drew Wyatt <drew.j.wyatt@gmail.com>
2022-01-01 22:04:39 -0500

    Initial Commit

---
+++ Shared/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  Shared
+//
+//  Created by Drew Wyatt on 1/1/22.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
